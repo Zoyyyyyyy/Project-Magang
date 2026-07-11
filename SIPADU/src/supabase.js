3c4f987549686868e2257133fadf51fb3bca37d1
Zoyyyyyyyyy <dutawijayarakyan19@gmail.com>
2026-07-11 14:14:01 +0700

    Update supabase.js

--- SIPADU/src/supabase.js
+++ SIPADU/src/supabase.js
@@ -3,8 +3,8 @@
  * Konfigurasi dan inisialisasi Supabase client
  * ============================================================ */
 
-const SUPABASE_URL = 'https://ordqbnaeiovvthusjzpx.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_Q8Y9pqQ8RDxHlf374sxlSw_LxXuqYFw';
+const SUPABASE_URL = 'https://xxxxxxxxxxxxxxxxxxx.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 const DUMMY_UUID = '00000000-0000-0000-0000-000000000000';
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
